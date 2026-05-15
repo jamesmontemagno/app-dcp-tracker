86f1b9852069c9953353c335e6d482cb5b7fe4e0
James Montemagno <jame.smontemagno>
2026-05-15 06:44:07 -0700

    Add raw assets and styles for the application
    - Created AboutAssets.txt to provide information on deploying raw assets with the application.
    - Added splash.svg for the splash screen design.
    - Introduced Colors.xaml to define a color palette for the application.
    - Established Styles.xaml to define styles for various UI components, including buttons, labels, and input fields, ensuring a consistent look and feel across the application.

---
+++ DCPTracker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DCPTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DCPTracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DCPTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
